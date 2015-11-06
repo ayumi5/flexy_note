977173f1bd09367164dcbe2b7047255bdd974d19
ayumi5 <ayumi.highroof@gmail.com>
2015-11-06 14:17:02 +1300

    make listing component

--- app/assets/javascripts/components/note.js.jsx
+++ app/assets/javascripts/components/note.js.jsx
@@ -3,7 +3,13 @@ var Note = React.createClass({
   render: function() {
     return (
       <div>
-        Hello World
+        <div className='panel panel-default col-sm-3 listing'>
+          <div className='panel-body'>
+            <p className='date'>2015/11/6</p>
+            <h4>test</h4>
+            <p>category</p>
+          </div>
+        </div>
       </div>
     )
   }
