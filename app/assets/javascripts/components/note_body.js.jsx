771a83057fdf1fff453fec13246af7871e3a6f15
ayumi5 <ayumi.highroof@gmail.com>
2015-11-12 14:20:59 +1300

    display formatted date on note

--- app/assets/javascripts/components/note_body.js.jsx
+++ app/assets/javascripts/components/note_body.js.jsx
@@ -1,13 +1,17 @@
 /** @jsx React.DOM */
 
 var NoteBody = React.createClass({
+  formatDate: function(dateStr){
+    date = new Date(dateStr)
+    return date.getFullYear() + '/' + date.getMonth() + '/' + date.getDate()
+  },
 
   render: function() {
     var noteId = this.props.note.id
     return (
       <div className='panel panel-default col-sm-3 listing'>
         <div className='panel-body'>
-          <p className='date col-sm-6 col-xs-10'>2015/11/6</p>
+          <p className='date col-sm-6 col-xs-10'>{this.formatDate(this.props.note.updated_at)}</p>
           <span className='action-icons'>
             <a onClick={this.onModalOpen}><i className='fa fa-file'></i></a>
             <a href={'/notes/' + noteId + '/edit'}><i className='fa fa-pencil-square-o fa-lg'></i></a>
