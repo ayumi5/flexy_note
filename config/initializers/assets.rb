6ca4ed24a2b4a1f3ae58796ea3a4fe7d67aa572d
ayumi5 <ayumi.highroof@gmail.com>
2016-01-31 15:41:38 +1300

    precompile files with other extensions

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( *.css )
+Rails.application.config.assets.precompile += %w( *.css, *.scss, *.sass, *.eot, *.ttf, *.woff, *.woff2, *.png, *.jpg, *.gif )
 Rails.application.config.assets.precompile += %w( *.js )
