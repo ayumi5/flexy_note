aa2361e28a4cffe3b90f04fea7dc76834a457ae4
ayumi5 <ayumi.highroof@gmail.com>
2016-12-21 20:45:52 +0900

    more fixing

--- app/models/note.rb
+++ app/models/note.rb
@@ -29,7 +29,7 @@ class Note < ActiveRecord::Base
             }
           },
           filter: { 
-            "and": [
+            "and" => [
               category_filter,
               { range: {
                 updated_at: { 
